0045d8356560ed26d765774a252db3715d5f8113
assemrh <assem.rh@gmail.com>
2024-09-15 05:39:33 +0300

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [CookieService]
 })
 export class AppComponent {
   title = 'salawat';
+  counter: number = 0;
+  cookieKey: string = 'counterValue';
+
+  constructor(private cookieService: CookieService) {}
+
+  ngOnInit(): void {
+    const savedValue = this.cookieService.get(this.cookieKey);
+    if (savedValue) {
+      this.counter = parseInt(savedValue, 10);
+    }
+  }
+
+  increment(): void {
+    this.counter++;
+    this.cookieService.set(this.cookieKey, this.counter.toString());
+  }
+
+  decrement(): void {
+    if (this.counter > 0) {
+      this.counter--;
+      this.cookieService.set(this.cookieKey, this.counter.toString());
+    }
+  }
+
+  reset(): void {
+    this.counter = 0;
+    this.cookieService.set(this.cookieKey, this.counter.toString());
+  }
 }
